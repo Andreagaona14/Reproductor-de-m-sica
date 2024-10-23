1e9325445cc95b736c71a058d8d77316d98d9a26
Andreagaona14 <andreitagaona05@gmail.com>
2024-10-23 09:25:04 -0500

    cambios en el playlist.js

--- js/playlist.js
+++ js/playlist.js
@@ -16,29 +16,29 @@ export const playList = [
 
 
     {
-        title: 'AL2 "El Aldeano"',
-        artist: 'Artista 2',
+        title: 'Amándote',
+        artist: 'AL2 "El Aldeano"',
         img:'img/img2.jpg',
         song:'music/Al2 El Aldeano - Amándote (Letra) - AL2 El Aldeano (youtube).mp3',
     },
 
     {
         title: 'AL2 "El Aldeano"',
-        artist: 'Artista 3',
+        artist: 'Como en una canción',
         img: 'img/img3.jpg',
         song:  'music/Al2 El Aldeano - Como en una Cancion (LETRA) - AL2 El Aldeano (youtube).mp3',
     },
 
     {
         title:'AL2 "El Aldeano"',
-        artist: 'Artista 4',
+        artist: 'Necesito Decirte una Cosa',
         img:  'img/img4.jpg',
         song:'music/Necesito Decirte una Cosa - AL2 El Aldeano (youtube).mp3',
   },
 
     {
         title: 'AL2 "El Aldeano"',
-        artist: 'Artista 5',
+        artist: 'Una Sola son ',
         img:  'img/img5.jpg',
         song:  'music/Una Sola Son ( LA ALDEA ON AIR ) - AL2 El Aldeano & Jhamy - AL2 El Aldeano (youtube).mp3',
     },
