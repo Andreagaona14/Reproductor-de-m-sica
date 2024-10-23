2eb77bd6841978b58ec295681991037c70b5f659
Andreagaona14 <andreitagaona05@gmail.com>
2024-10-23 08:28:56 -0500

    Cambios en el app.js

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-import { playList } from "./playlist"
+const audio = document.getElementById('audio');
+const play = document.getElementById('play');
+const pause = document.getElementById('pause');
+const forward = document.getElementById('forward');
+const rewind = document.getElementById('rewind');
+const stop = document.getElementById('stop');
 
-const audio = document.getElementById('audio')
-const play = document.getElementById('play')
-const pause = document.getElementById('pause')
-const forward = document.getElementById('forward')
-const rewind = document.getElementById('rewind')
-const stop = document.getElementById('stop')
+play.addEventListener('click', () => {
+    if (audio.paused) {
+        audio.play();
+        play.classList.remove('bx-play');
+        play.classList.add('bx-pause');
+    } else {
+        audio.pause();
+        play.classList.remove('bx-pause');
+        play.classList.add('bx-play');
+    }
+});
 
-play.addEventListener('click', () => audio.play())
-
-play.addEventListener('click', () => audio.pause())
-
-rewind.addEventListener('click', ()=> audio.currentTime -= 10)
-
-forward.addEventListener('click', ()=> audio.currentTime += 10)
-
-stop.addEventListener('click', () =>{
-    audio.pause()
-    audio.currentTime = 0
-})
+rewind.addEventListener('click', () => audio.currentTime -= 10);
+forward.addEventListener('click', () => audio.currentTime += 10);
+stop.addEventListener('click', () => {
+    audio.pause();
+    audio.currentTime = 0;
+    play.classList.remove('bx-pause');
+    play.classList.add('bx-play');
+});
