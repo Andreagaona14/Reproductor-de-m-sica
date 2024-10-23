85362b37528485c28a269b6b491926118397a9e9
Andreagaona14 <andreitagaona05@gmail.com>
2024-10-23 08:12:24 -0500

    Agregue las canciones y las imagenes

--- playlist.js
+++ playlist.js
@@ -1,36 +1,36 @@
 export const playList = [
     {
-        title: 'Song 1',
-        artist: 'Artist 1',
-        img:
-        song:
+        title: 'AL2 "El Aldeano"',
+        artist: 'Artista 1',
+        img: 'img/img1.jpg',
+        song: 'music/Al2 El Aldeano - A Veces (LETRA) - AL2 El Aldeano (youtube).mp3',
     },
 
     {
-        title: 'Song 1',
-        artist: 'Artist 1',
-        img:
-        song:
+        title: 'AL2 "El Aldeano"',
+        artist: 'Artista 2',
+        img:'img/img2.jpg',
+        song:'music/Al2 El Aldeano - Amándote (Letra) - AL2 El Aldeano (youtube).mp3',
     },
 
     {
-        title: 'Song 1',
-        artist: 'Artist 1',
-        img:
-        song:
+        title: 'AL2 "El Aldeano"',
+        artist: 'Artista 3',
+        img: 'img/img3.jpg',
+        song:  'music/Al2 El Aldeano - Como en una Cancion (LETRA) - AL2 El Aldeano (youtube).mp3',
     },
 
     {
-        title: 'Song 1',
-        artist: 'Artist 1',
-        img:
-        song:
-    },
+        title:'AL2 "El Aldeano"',
+        artist: 'Artista 4',
+        img:  'img/img4.jpg',
+        song:'music/Necesito Decirte una Cosa - AL2 El Aldeano (youtube).mp3',
+  },
 
     {
-        title: 'Song 1',
-        artist: 'Artist 1',
-        img:
-        song:
+        title: 'AL2 "El Aldeano"',
+        artist: 'Artista 5',
+        img:  'img/img5.jpg',
+        song:  'music/Una Sola Son ( LA ALDEA ON AIR ) - AL2 El Aldeano & Jhamy - AL2 El Aldeano (youtube).mp3',
     },
 ]
