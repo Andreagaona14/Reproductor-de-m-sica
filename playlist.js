d3aab39b3eb66147571479d1e793f5793f5799f9
Andreagaona14 <andreitagaona05@gmail.com>
2024-10-23 07:14:55 -0500

    Creación de playlist.js

---
+++ playlist.js
@@ -0,0 +1,36 @@
+export const playList = [
+    {
+        title: 'Song 1',
+        artist: 'Artist 1',
+        img:
+        song:
+    },
+
+    {
+        title: 'Song 1',
+        artist: 'Artist 1',
+        img:
+        song:
+    },
+
+    {
+        title: 'Song 1',
+        artist: 'Artist 1',
+        img:
+        song:
+    },
+
+    {
+        title: 'Song 1',
+        artist: 'Artist 1',
+        img:
+        song:
+    },
+
+    {
+        title: 'Song 1',
+        artist: 'Artist 1',
+        img:
+        song:
+    },
+]
